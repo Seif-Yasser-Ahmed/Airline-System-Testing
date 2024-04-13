3bdae1f66addb9fb0b4d54b16b19ffc4a1d89088
ZeynatGhallab <89994856+ZeynatGhallab@users.noreply.github.com>
2024-04-13 16:22:53 +0300

    Airport.java

--- src/main/java/com/example/ainline_system_team5/Airport.java
+++ src/main/java/com/example/ainline_system_team5/Airport.java
@@ -5,5 +5,29 @@ import java.util.Stack;
 
 public class Airport {
     public static List<Flight> flights=new Stack<Flight>();
+    private List<Booking> bookings;
+    
+    private String code;
+    private String name;
+    private String country;
+   
+
+    public Airport(String code, String name, String country) {
+        this.code = code;
+        this.name = name;
+        this.country = country;
+    }
+    
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
+    
+    public void removeBooking(Booking booking) {
+        this.bookings.remove(booking);
+    }
+    
+    public String getAirportInfo() {
+        return "Airport Code: " + code + ", Name: " + name + ", Country: " + country;
+    }
 
 }
